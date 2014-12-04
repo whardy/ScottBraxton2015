eb97a11476e2f26ae618d5d550e355396f8a8d3b
whardy <walterhardy+github@gmail.com>
2014-12-03 22:26:39 -0500

    added photo credits and moved countdown
    added credits for Kyle Monroe and Walter Hardy and moved countdown below
    the buttons on the homepage

--- assets/js/soon/custom.js
+++ assets/js/soon/custom.js
@@ -43,7 +43,7 @@ COMMING SOON PAGE
             seconds.html('<h1>'+s+'</h1><p>Second'+(s>1?'s':''),'</p>');
             setTimeout(setDate, 1000);
 
-            message.html('Countdown until the campaign:');
+            message.html('Countdown to the campaign:');
         }
     }
 })(jQuery);
